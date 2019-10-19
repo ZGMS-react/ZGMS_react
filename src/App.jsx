ff43b55ec11343c081676c2c1da415d802563991
moguanshan99 <QYP999901@163.com>
2019-10-19 18:53:31 +0800

    登录界面测试

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { Component, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Spin } from 'antd';
-
-import Login from '@conts/login';
 import NotMatch from '@comps/not-match';
 import routes from './config/routes';
 
@@ -11,13 +9,6 @@ class App extends Component {
     // Suspense用于懒加载
     return <Suspense fallback={<Spin size="large"/>}>
       <Router>
-        {/*<Switch>*/}
-        {/*  <Route path="/" exact component={Home}/>*/}
-        {/*  <Route path="/login" exact component={Login}/>*/}
-        {/*</Switch>*/}
-        <Switch>
-          <Route path="/login" component={Login} exact/>
-          <BasicLayout>
             <Switch>
               {
                 routes.map((route, index) => {
@@ -28,8 +19,7 @@ class App extends Component {
               {/* 不写path 就是匹配所有路径 */}
               <Route component={NotMatch}/>
             </Switch>
-          </BasicLayout>
-        </Switch>
+       
       </Router>
     </Suspense>
   }
