e7af3e39300978be672eed3178de9e9e63da4766
moguanshan99 <QYP999901@163.com>
2019-10-19 20:31:39 +0800

    login组件第一次提交

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-class Login extends React.Component{
-    render(){
+import './index.less'
+class Login extends React.Component {
+    render() {
         return (
-            <div>登录界面</div>
+            <div className="login">
+                <section className="login_main">
+                    <div className="login_logo">
+                        <img className="login_img" src="./images/login.png" alt=""/>
+                        <span>美团民宿</span>
+                    </div>
+                    <div className="login_content"></div>
+                </section>
+                <footer className="login_footer">&copy;2019 美团网团购 meituan.com 京ICP证070791号 京公网安备11010502025545号</footer>
+
+            </div>
         )
     }
 }
