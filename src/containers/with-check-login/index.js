a9599ae986aca69137fddd6a6bcf8e7d33a52b8a
moguanshan99 <QYP999901@163.com>
2019-10-21 19:20:59 +0800

    修改

--- src/containers/with-check-login/index.js
+++ src/containers/with-check-login/index.js
@@ -8,7 +8,7 @@ import {Redirect} from 'react-router-dom'
 *    --3.如果已经登陆了，再访问登录页面就定向到该页面，不用再登录
 */
 
-//定义一个高阶组件用来进行登录验证（是因为每个组件都要进行这个登录验证，为实现代码的复用，故定义高阶组件来实现）
+//定义一个高阶组件用来进行登录验证（为实现代码的复用，故定义高阶组件来实现）
 
 export default function withCheckLogin(WrappedComponent) {
 
@@ -22,40 +22,15 @@ export default function withCheckLogin(WrappedComponent) {
 
             render() {
 
-                // const {location,token,history,match}=this.props;
-                // const {pathname}=location;
-
               //解决高阶组件丢失children属性问题
                 const {token,...rest}=this.props;//除了token，剩余的其他属性全部打包到rest中
                 const {location:{pathname}}=rest;
-                //登录验证
-                /*
-                * 1.首先判断当前地址是否为'/login'
-                *     --由路由（<Route/>）加载的组件都有路由的三大属性【history，location，match】,而在location中又有一个pathname属性                        ，值为当前的地址
-                *     --因为返回的新的组件（即Login组件外面包裹着一个withCheckLogin组件），而新组建是被路由组件加载的，故新组件身上有路由                       的三大属性，故可以直接使用location，但是Login组件不是通过路由加载的，因此已经没有路由的三大属性了，但是可以通过                         withCheck组件传递
-                * 2.如果用户已经登陆了，跳转到'/'
-                *     --如何判断用户已经登陆了？
-                *        ---1).根据token，如果有token证明用户已经登陆了
-                *        ---2).如何获取到token  通过connect高阶组件将token传过来
-                * 3.如果用户没有登录，则不变
-                */
-
-                // if (pathname==='/login'){
-                //     if (token){
-                //           return <Redirect to="/"/>
-                //     }
-                // }else{
-                //     if (!token){
-                //         return <Redirect to="/login"/>
-                //     }
-                // }
 
                 if (pathname==='/login'&&token) return <Redirect to="/"/>;
                 if (pathname!=='/login'&&!token) return <Redirect to="/login"/>;
 
 
                 //Login组件已经没有路由的三大属性了，故需要通过新组建传递过去
-                // return <WrappedComponent history={history} location={location} match={match}/>;
                 return <WrappedComponent {...rest}/> //解包
             }
         }
